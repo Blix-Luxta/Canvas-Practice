ed0eb52ae51e60515c927a35f794984766e743e9
Blix-Luxta <blix.aux@gmail.com>
2023-05-24 15:57:25 -0300

    cambiando funcionalidad y agregando cambio de color

--- index.js
+++ index.js
@@ -1,30 +1,6 @@
 let paintPalette = document.getElementById("paint-palette")
 
-// Constructor de paletas
-class paintBucket {
-    constructor (color, clase){
-        this.color = color
-        this.clase = clase
-    }
-    createDiv(){
-        let content = document.createElement("div")
-        content.className = this.clase
-        content.style.backgroundColor = this.color
-        content.style.border = "2px solid black"
 
-        paintPalette.append(content)
-    }
-}
-
-const redColor = new paintBucket("red", "color-bucket")
-const greenColor = new paintBucket("green", "color-bucket")
-const blueColor = new paintBucket("blue", "color-bucket")
-const yellowColor = new paintBucket("yellow", "color-bucket")
-
-redColor.createDiv()
-greenColor.createDiv()
-blueColor.createDiv()
-yellowColor.createDiv()
 
 // variables del canvas
 
@@ -34,51 +10,60 @@ const context = canvas.getContext("2d")
 canvas.width = innerWidth - 60
 canvas.height = 400
 
-let initialX
-let initialY
 
-let drawColor = "black"
 let lineWidth = "2"
 let isDrawing = false
+let prevX = null
+let prevY = null
+
+// paletas de colores
+
+let color = document.querySelectorAll(".color-bucket")
+color = Array.from(color)
+color.forEach(clr => {
+    clr.addEventListener("click", ()=>{
+        context.strokeStyle = clr.dataset.color
+    })
+})
 
 //Funcionalidad
 
+canvas.addEventListener("mousedown", (e) => isDrawing = true)
+canvas.addEventListener("mouseup", (e) => isDrawing = false)
 
-canvas.addEventListener("touchstart", start, false)
-canvas.addEventListener("touchmove", draw, false)
-canvas.addEventListener("touchend", stop, false)
-canvas.addEventListener("mousedown", start, false)
-canvas.addEventListener("mousemove", draw, false)
-canvas.addEventListener("mouseup", stop, false)
+canvas.addEventListener("mousemove", (e) => {
+    if(prevX == null || prevY == null || !isDrawing){
+        prevX = e.clientX - canvas.offsetLeft
+        prevY = e.clientY - canvas.offsetTop
+        return
+    }
 
+    let currentX = e.clientX - canvas.offsetLeft
+    let currentY = e.clientY - canvas.offsetTop
 
-function start(event) {
-    isDrawing = true
     context.beginPath()
-    context.moveTo(event.clientX - canvas.offsetLeft, event.clientY - canvas.offsetTop)
-}
+    context.moveTo(prevX, prevY)
+    context.lineTo(currentX, currentY)
+    context.lineCap = "round"
+    context.lineJoin = "round"
+    context.stroke()
 
-function draw(event) {
-    if(isDrawing){
-        context.lineTo(event.clientX - canvas.offsetLeft, event.clientY - canvas.offsetTop)
-        context.strokeStyle = drawColor
-        context.lineWidth = lineWidth
-        context.lineCap = "round"
-        context.lineJoin = "round"
-        context.stroke()
-    }
+    prevX = currentX
+    prevY = currentY
+})
 
+let theInput = document.getElementById("color-picker");
 
-}
-
-function stop(event){
-    if (isDrawing) {
-        context.stroke()
-        context.closePath()
-        isDrawing = false
-
-    }
-    
-}
+theInput.addEventListener("input", function(){
+    theColor = theInput.value;
+    context.strokeStyle = theColor;
+}, false);
 
 
+let pencilWidth = document.getElementById("pencil-range");
+
+pencilWidth.addEventListener("input", function(){
+    lineW = pencilWidth.value;
+    lineWidth = lineW;
+    context.lineWidth = lineW
+}, false);
